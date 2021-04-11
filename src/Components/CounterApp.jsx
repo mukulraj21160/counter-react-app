d8b187e5f7ddc6e31fe86b760f15d502b0d464b4
Mukul Raj <mukulraj21160@gmail.com>
2021-04-11 21:47:44 +0530

    [Mukul] completes-counter-app

--- src/Components/CounterApp.jsx
+++ src/Components/CounterApp.jsx
@@ -1,29 +1,84 @@
 import React, { useState } from "react";
-import { Card, IconButton, makeStyles, Typography } from "@material-ui/core";
+import {
+  Card,
+  Grid,
+  IconButton,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import RemoveCircleIcon from "@material-ui/icons/RemoveCircle";
 import RefreshIcon from "@material-ui/icons/Refresh";
 
 const style = makeStyles((theme) => ({
   main: {
-    background:
-      "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
-    // background: "rgb(131,58,180)",
-    color: "white",
-    height: "100vh",
+    fontFamily: theme.typography.body1.fontFamily,
+    [theme.breakpoints.up("xs")]: {
+      background:
+        "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+    },
+
+    [theme.breakpoints.up("md")]: {
+      background:
+        "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+    },
+    [theme.breakpoints.up("lg")]: {
+      background:
+        "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+    },
   },
   card: {
-    backgroundColor: "#000000",
-    color: "white",
-    width: "30%",
-    margin: "0px 150px 150px 450px",
-    padding: "20px 20px 20px 20px",
-    textAlign: "center",
-    height: "50%",
+    fontFamily: theme.typography.body1.fontFamily,
+    [theme.breakpoints.up("xs")]: {
+      color: "white",
+      width: "80%",
+      margin: "20% 0% 10% 0%",
+      marginLeft: "32px",
+      textAlign: "center",
+      height: "30%",
+    },
+
+    [theme.breakpoints.up("md")]: {
+      color: "white",
+      width: "30%",
+      margin: "10% 0% 10% 0%",
+      padding: "20px 20px 20px 20px",
+      textAlign: "center",
+      height: "50%",
+    },
+    [theme.breakpoints.up("lg")]: {
+      color: "white",
+      width: "30%",
+      margin: "10% 0% 10% 0%",
+      padding: "20px 20px 20px 20px",
+      textAlign: "center",
+      height: "50%",
+    },
   },
   heading: {
-    fontWeight: "bold",
-    fontSize: "32px",
+    fontFamily: theme.typography.body1.fontFamily,
+    [theme.breakpoints.up("xs")]: {
+      fontWeight: "bold",
+      fontSize: "22px",
+    },
+
+    [theme.breakpoints.up("md")]: {
+      fontWeight: "bold",
+      fontSize: "32px",
+    },
+    [theme.breakpoints.up("lg")]: {
+      fontWeight: "bold",
+      fontSize: "32px",
+    },
   },
 }));
 
@@ -50,37 +105,45 @@ const CounterApp = () => {
   const classes = style();
   return (
     <div className={classes.main}>
-      <Card className={classes.card}>
-        <Typography className={classes.heading}>Counter Application</Typography>
-        <br />
-        <br />
-        <br />
-        <div>
-          <Typography>
-            <IconButton
-              variant="contained"
-              color="secondary"
-              onClick={increment}
-            >
-              <AddCircleIcon />
+      <Grid container item>
+        <Grid item lg={4} md={4} sm={2}></Grid>
+        <Grid item lg={4} md={4} sm={8} className={classes.card}>
+          <Card>
+            <Typography className={classes.heading}>
+              Counter Application
+            </Typography>
+            <br />
+            <br />
+            <br />
+            <div>
+              <Typography>
+                <IconButton
+                  variant="contained"
+                  color="secondary"
+                  onClick={increment}
+                >
+                  <AddCircleIcon />
+                </IconButton>
+                {counter}
+                <IconButton
+                  varient="contained"
+                  color="secondary"
+                  onClick={decrement}
+                >
+                  <RemoveCircleIcon />
+                </IconButton>
+              </Typography>
+            </div>
+            <br />
+            <br />
+            <IconButton varient="contained" color="secondary" onClick={reset}>
+              <RefreshIcon />
+              reset
             </IconButton>
-            {counter}
-            <IconButton
-              varient="contained"
-              color="secondary"
-              onClick={decrement}
-            >
-              <RemoveCircleIcon />
-            </IconButton>
-          </Typography>
-        </div>
-        <br />
-        <br />
-        <IconButton varient="contained" color="secondary" onClick={reset}>
-          <RefreshIcon />
-          reset
-        </IconButton>
-      </Card>
+          </Card>
+        </Grid>
+        <Grid item lg={4} md={4} sm={2}></Grid>
+      </Grid>
     </div>
   );
 };
